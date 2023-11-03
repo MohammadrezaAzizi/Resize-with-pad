e65fe7f490d2b390ffe632435b5faab1fbfe2cd6
Mohammadreza Azizi <azizigenius@gmail.com>
2023-11-03 17:13:04 +0330

    "Fixed some errors"

--- base_function.py
+++ base_function.py
@@ -1,9 +1,10 @@
 import numpy as np 
 import tensorflow as tf
+import cv2
 
 image = tf.keras.utils.load_img(r"E:\Heart\FINAL Cropped\Cropped\ABASNIA, GAFAR_Run30_1.jpg", grayscale=False)
 
-def resize_with_pad(image, target_width, target_height, pad_range:tuple):
+def resize_with_pad(image, target_width, target_height, pad_min, pad_max):
     """
     This functions checks out the width & height in target & current image and ratio. We wanna make sure that the ratio is intact.
     1- Get the current width and height. 
@@ -13,12 +14,17 @@ def resize_with_pad(image, target_width, target_height, pad_range:tuple):
 
     """
     height, width, depth = tf.shape(image)
-    pad = np.random.random()*pad_range
-    h_pad = pad * target_height/target_width
-    w_pad = pad * target_width/target_height
-    np.pad(image, ((int(h_pad/2), int(h_pad/2)), (int(w_pad/2), int(w_pad/2)), (0, 0)),
+    pad = np.random.randint(pad_min, pad_max)
+    print(pad)
+    h_pad = pad * (target_height/target_width)
+    w_pad = pad * (target_width/target_height)
+    img = np.pad(image, ((int(h_pad/2), int(h_pad/2)), (int(w_pad/2), int(w_pad/2)), (0, 0)),
             mode='constant',constant_values=0)
-    img = tf.image.resize(image, [target_height, target_width, depth], preserve_aspect_ratio=True)
-    return img
+    img = tf.image.resize(img, [target_height, target_width], preserve_aspect_ratio=True)
+    img = tf.image.per_image_standardization(img)
+    cv2.imshow("img", np.array(img))
+    cv2.imshow("image", np.array(image))
+    cv2.waitKey(0)
+    cv2.destroy_all_windows()
 
-resize_with_pad(image, 200, 50, (10,25))
+resize_with_pad(image, 200, 500, 50, 100)
